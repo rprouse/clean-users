735333356b0a66e4f9944e0594f5d7eb6a066e39
Rob Prouse <rob@prouse.org>
2017-08-26 10:42:39 -0400

    Initial checking

---
+++ README.md
@@ -0,0 +1,8 @@
+### Clean Users
+
+This goes through the users in the NUnit organization and flags members that have not contributed recently.
+
+### Running
+
+1. Go to [GitHub Settings | Personal access tokens](https://github.com/settings/tokens) and generate a token.
+2. Create a file called `.env` in this directory and add the token to it, `GITHUB_TOKEN=<add token here>`
---
+++ app.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+var request = require("request");
+require('dotenv').config();
+
+var token = process.env.GITHUB_TOKEN;
+
+var options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    authorization: 'token ' + token,
+    'cache-control': 'no-cache',
+    'User-Agent': 'NodeJS'
+  }
+};
+
+request('https://api.github.com/orgs/nunit/repos?per_page=50', options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  var json = JSON.parse(body);
+  for (repo of json) {
+    console.log(repo.name);
+  }
+});
+
+// request('https://api.github.com/orgs/nunit/members?per_page=100', options, function (error, response, body) {
+//   if (error) throw new Error(error);
+
+//   var users = JSON.parse(body)
+//                   .map(function(u) { return u.login });
+//   for (user of users) {
+//     console.log(user);
+//   }
+// });
+
+// request('https://api.github.com/repos/nunit/nunit/stats/contributors', options, function (error, response, body) {
+//   if (error) throw new Error(error);
+
+//   var json = JSON.stringify(JSON.parse(body), null, 2);
+//   console.log(json);
+// });
