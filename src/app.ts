5aae147fb9fb17068c643b2318579db216cebce2
Rob Prouse <rob.prouse@bentley.com>
2017-08-30 08:19:06 -0400

    Switch to modules and namespaces to clean up the code

--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,17 @@
 /// <reference path="GitHub.ts" />
 
-import * as request from 'request-promise';
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-const token = process.env.GITHUB_TOKEN;
-if(!token) {
-  console.error("You must set the environment variable GITHUB_TOKEN. See the README.");
-  process.exit(-1);
-}
-
-class User {
-  constructor(public login: string, public last_commit: number) {}
-}
-
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    authorization: 'token ' + token,
-    'cache-control': 'no-cache',
-    'User-Agent': 'NodeJS'
-  }
-};
-
-async function getRepositories(): Promise<GitHub.Repository[]> {
-  let json = await request('https://api.github.com/orgs/nunit/repos?per_page=50', options);
-  return JSON.parse(json);
-}
-
-async function getMembers(): Promise<User[]> {
-  let json = await request('https://api.github.com/orgs/nunit/members?per_page=100', options);
-  let members: GitHub.Member[] = JSON.parse(json);
-  return members.map(function(u: GitHub.Member) { return new User(u.login, 0) });
-}
-
-async function getStatistics(repository: GitHub.Repository): Promise<GitHub.Statistic[]> {
-  let json = await request('https://api.github.com/repos/' + repository.full_name + '/stats/contributors', options);
-  return JSON.parse(json);
-}
+import * as github from './Requests';
 
 async function main() {
   try {
-    let repositories = await getRepositories();
-    let users = await getMembers();
+    let repositories = await github.getRepositories();
+    let users = await github.getMembers();
 
     for (let repo of repositories) {
-      var stats = await getStatistics(repo);
-      console.log(stats);
+      let stats = await github.getStatistics(repo);
+      for(let stat of stats) {
+        console.log(stat.author.login);
+      }
     }
   } catch(err) {
     console.error('Error: ', err.message);
