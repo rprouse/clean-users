1b1d85515f6ac0943e24fcfd44cb868b08b8bb80
Rob Prouse <rob@prouse.org>
2017-09-06 11:29:29 -0400

    Refactored to return undefined from failed API calls

--- src/App.ts
+++ src/App.ts
@@ -10,51 +10,52 @@ function latestCheckin(stat: GitHub.Statistic): number {
 async function main() {
   try {
     let repositories = await github.getRepositories();
-    if(repositories === undefined || !(repositories instanceof Array) || repositories.length === 0) {
+    if(repositories !== undefined) {
+      let users = await github.getMembers();
+      if(users !== undefined) {
+
+        let failed = false;
+        for (let repo of repositories) {
+          let stats = await github.getStatistics(repo);
+          if(stats === undefined) {
+            console.warn("Failed to fetch stats for " + repo.name);
+            failed = true;
+            continue;
+          }
+
+          for(let stat of stats) {
+            let last = users.get(stat.author.login);
+            if(last !== undefined) {
+              let statLast = latestCheckin(stat);
+              if(statLast > last) {
+                users.set(stat.author.login, statLast);
+              }
+            }
+          }
+        }
+
+        // The GitHub stats API is expensive and fails, so you need to request,
+        // then they will be generated and you will get results on your second
+        // request
+        if(failed) {
+          console.warn("Wait a minute for stats to generate and rerun.")
+          process.exit(1);
+        }
+
+        for (let key of users.keys()) {
+          console.log(key + ': ' + users.get(key));
+        }
+      } else {
+        console.error("Failed to fetch users for the organization.");
+        process.exit(-3);
+      }
+    } else {
       console.error("Failed to fetch repositories for the organization.");
       process.exit(-2);
     }
-
-    let users = await github.getMembers();
-    if(users === undefined || !(users instanceof Map) || users.size === 0) {
-      console.error("Failed to fetch users for the organization.");
-      process.exit(-3);
-    }
-
-    let failed = false;
-    for (let repo of repositories) {
-      let stats = await github.getStatistics(repo);
-      if(stats === undefined || !(stats instanceof Array) || stats.length === 0) {
-        console.warn("Failed to fetch stats for " + repo.name);
-        failed = true;
-        continue;
-      }
-
-      for(let stat of stats) {
-        let last = users.get(stat.author.login);
-        if(last !== undefined) {
-          let statLast = latestCheckin(stat);
-          if(statLast > last) {
-            users.set(stat.author.login, statLast);
-          }
-        }
-      }
-    }
-
-    // The GitHub stats API is expensive and fails, so you need to request,
-    // then they will be generated and you will get results on your second
-    // request
-    if(failed) {
-      console.warn();
-      console.warn("Wait a minute for stats to generate and rerun.")
-      process.exit(1);
-    }
-
-    for (let key of users.keys()) {
-      console.log(key + ': ' + users.get(key));
-    }
   } catch(err) {
     console.error('Error: ', err.message);
+    process.exit(-10);
   }
 }
 
--- src/Requests.ts
+++ src/Requests.ts
@@ -20,29 +20,32 @@ const options = {
   }
 };
 
-export async function getRepositories(): Promise<GitHub.Repository[]> {
+export async function getRepositories(): Promise<GitHub.Repository[] | undefined> {
   let json = await request('https://api.github.com/orgs/nunit/repos?per_page=50', options);
-  return JSON.parse(json);
+  let repos = JSON.parse(json);
+  if(repos === undefined || !(repos instanceof Array) || repos.length === 0)
+    return undefined;
+  return repos;
 }
 
 // The map key is the user login, number is the last commit as a unix timestamp
-export async function getMembers(): Promise<Map<string, number>> {
+export async function getMembers(): Promise<Map<string, number> | undefined> {
   let json = await request('https://api.github.com/orgs/nunit/members?per_page=100', options);
   let members: GitHub.Member[] = JSON.parse(json);
   let users: Map<string, number> = new Map<string, number>();
-  if(members === undefined || !(users instanceof Array) || members.length === 0)
-    return users;
+  if(members === undefined || !(members instanceof Array) || members.length === 0)
+    return undefined;
   for(let member of members) {
     users.set(member.login, 0);
   }
   return users;
 }
 
-export async function getStatistics(repository: GitHub.Repository): Promise<GitHub.Statistic[]> {
+export async function getStatistics(repository: GitHub.Repository): Promise<GitHub.Statistic[] | undefined> {
   let json = await request('https://api.github.com/repos/' + repository.full_name + '/stats/contributors', options);
   let stats: GitHub.Statistic[] = JSON.parse(json);
   if(stats === undefined || !(stats instanceof Array) || stats.length === 0)
-    return stats;
+    return undefined;
 
   for(let stat of stats) {
     stat.weeks = stat.weeks.filter((w: GitHub.Week) => w.a !== 0 && w.c !== 0 && w.d !== 0);
